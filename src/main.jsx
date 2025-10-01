3c8900796f58335b310dfb78ae5a89bb07f03ff1
Jack <yezdanshermm@gmail.com>
2025-10-01 10:06:41 +0200

    Init: Routingcongif

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
 
+import App from "./App";
 import Übersicht from "./pages/Übersicht.jsx";
 import Isi from "./pages/Isi.jsx";
 import JackDashboard from "./pages/JackDashboard.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path= "/übersicht" element={<Übersicht />} />
+      <Route path="/übersicht" element={<Übersicht />} />
       <Route path="/jack" element={<JackDashboard />} />
       <Route path="/isi" element={<Isi />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
